df403de662a2653c541b4fb5d480f674c5270656
Heorhii <georgij.getmanu@gmail.com>
2025-08-01 13:53:34 +0300

    First test, week 10

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # AQA_Training
+
+Required
+
+- node
+- playwright
+
+Commands
+
+run all - npx playwright test
+run some test - npx playwright test {test dir/...}
+run test with visual npx playwright test --headed
---
+++ tests/1-login.spec.ts
@@ -0,0 +1,19 @@
+import { test, expect } from '@playwright/test';
+
+const user = {
+  name: 'Jane Doe',
+  email: 'customer@practicesoftwaretesting.com',
+  password: 'welcome01'
+}
+
+test('login', async ({ page }) => {
+  await page.goto('https://practicesoftwaretesting.com/auth/login');
+  await page.locator('#email').fill(user.email);
+  await page.locator('#password').fill(user.password);
+  await page.locator('input.btnSubmit').click();
+  await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
+  // line 11 i think more reliable
+  // await expect(page.getByText('My account')).toBeVisible();
+  await expect(page.locator('[data-test="page-title"]')).toContainText('My account');
+  await expect(page.locator('#menu')).toContainText(user.name);
+});
