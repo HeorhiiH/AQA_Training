4ffbd63a16a71428a4727d2083c6da5d1c031c24
Heorhii <georgij.getmanu@gmail.com>
2025-08-06 00:10:50 +0300

    Change locator for the login button

--- tests/1-login.spec.ts
+++ tests/1-login.spec.ts
@@ -10,7 +10,7 @@ test('login', async ({ page }) => {
   await page.goto('https://practicesoftwaretesting.com/auth/login');
   await page.locator('#email').fill(user.email);
   await page.locator('#password').fill(user.password);
-  await page.locator('input.btnSubmit').click();
+  await page.getByTestId('login-submit').click();
   await expect(page).toHaveURL('https://practicesoftwaretesting.com/account');
   // line 11 i think more reliable
   // await expect(page.getByText('My account')).toBeVisible();
