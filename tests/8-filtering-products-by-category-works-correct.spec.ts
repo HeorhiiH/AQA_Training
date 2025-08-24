d4a052777d5b4967b063bb7d3f79fe54e1d99174
Heorhii <georgij.getmanu@gmail.com>
2025-08-24 17:39:35 +0300

    Pull after rename file in GIT

--- tests/8-filtering-products-by-category-works-correct.spec.ts
+++ tests/8-filtering-products-by-category-works-correct.spec.ts
@@ -4,53 +4,54 @@ import { PowerTools } from '../Enum/homePageEnum';
 
 
 
-test('login', async ({ page }) => {
-  test.skip(!!process.env.GITHUB_ACTIONS, 'Skip it in GitHub Actions');
+// test('login', async ({ page }) => {
+//   test.skip(!!process.env.GITHUB_ACTIONS, 'Skip it in GitHub Actions');
 
-  const homePage = new HomePage(page);
-  await page.goto('');
-  await homePage.checkboxItem.getByText(PowerTools.Sander).click();
-  await homePage.responseWaiting('by_category=01K3E6295WEWVWBQ2RT1NTTPE8');
-  const namesArray = await homePage.productName.allInnerTexts();
-  const checkFilterParam = function (arr: string[]) {
-    return arr.every(name => name.includes("Sander"));
-  }
-  expect(checkFilterParam(namesArray)).toBeTruthy();
-});
+//   const homePage = new HomePage(page);
+//   await page.goto('');
+//   await homePage.checkboxItem.getByText(PowerTools.Sander).click();
+//   await homePage.responseWaiting('by_category=01K3E6295WEWVWBQ2RT1NTTPE8');
+//   const namesArray = await homePage.productName.allInnerTexts();
+//   const checkFilterParam = function (arr: string[]) {
+//     return arr.every(name => name.includes("Sander"));
+//   }
+//   expect(checkFilterParam(namesArray)).toBeTruthy();
+// });
 
 
 
 //Work on local environment but has a problem on CI with enum import
-// test.describe('Filtering by categories', () => {
+//No, git ignored rename Enum file, try again after rename file
+test.describe('Filtering by categories', () => {
 
-//   const filters = [
-//     {
-//       path: "by_category=01K3E2MCJEP5TERTNDRP8ZVWPB",
-//       label: PowerTools.Sander
-//     }
-//   ];
+  const filters = [
+    {
+      path: 'by_category=01K3E6295WEWVWBQ2RT1NTTPE8',
+      label: PowerTools.Sander
+    }
+  ];
 
-//   filters.forEach(({ path, label }) => {
+  filters.forEach(({ path, label }) => {
 
-//     test(`should filter products by category: ${label}`, async ({ page }) => {
-//       test.skip(!!process.env.GITHUB_ACTIONS, 'Skip in GitHub Actions');
+    test(`should filter products by category: ${label}`, async ({ page }) => {
+      test.skip(!!process.env.GITHUB_ACTIONS, 'Skip in GitHub Actions');
 
-//       // answer log
-//       //Sometimes website change id of the category .... :( 
-//       // page.on('response', res => {
-//       //   console.log('➡️ Response:', res.url(), res.status());
-//       // });
+      // answer log
+      //Sometimes website change id of the category .... :( 
+      // page.on('response', res => {
+      //   console.log('➡️ Response:', res.url(), res.status());
+      // });
 
-//       const homePage = new HomePage(page);
+      const homePage = new HomePage(page);
 
-//       await page.goto('');
-//       await homePage.checkboxItem.getByText(label).click();
+      await page.goto('');
+      await homePage.checkboxItem.getByText(label).click();
 
-//       await homePage.responseWaiting(path);
+      await homePage.responseWaiting(path);
 
-//       const namesArray = await homePage.productName.allInnerTexts();
+      const namesArray = await homePage.productName.allInnerTexts();
 
-//       expect(namesArray.every(name => name.includes(label))).toBeTruthy();
-// });
-//   });
-// });
+      expect(namesArray.every(name => name.includes(label))).toBeTruthy();
+});
+  });
+});
